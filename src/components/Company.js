9476cc685b42fdc78008608a228dd81e71077559
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-01-14 17:42:23 +0330

    fixed bug style active

--- src/components/Company.js
+++ src/components/Company.js
@@ -9,7 +9,7 @@ import { getCoin } from "../services/api";
 import Loader from "./Loader";
 
 const Company = () => {
-  const [changeClass, setChangeClass] = useState(false);
+  const [selectedCoin, setSelectedCoin] = useState(null);
   const [coins, setCoins] = useState([]);
 
   useEffect(() => {
@@ -20,32 +20,22 @@ const Company = () => {
     fetchAPI();
   }, []);
 
-  const myHandler = () => {
-    setChangeClass(true);
-    console.log("trued");
-  };
-
-  const myHandler2 = () => {
-    console.log();
-  };
-
   return (
     <>
-      {coins.length ? (
+      {coins.length > 0 ? (
         coins.map((coin) => (
           <Row
             className={
-              changeClass
+              selectedCoin === coin.id
                 ? "p-2 border-top d-flex align-items-center company-list-single-active"
                 : "p-2 border-top d-flex align-items-center company-list-single"
             }
             onClick={() => {
-              myHandler();
-              myHandler2();
+              setSelectedCoin(coin.id);
             }}
             key={coin.id}
           >
-            <Col xxl="3" xl="3" lg="4" md="4" sm="2" xs="2">
+            <Col xxl="2" xl="2" lg="3" md="3" sm="2" xs="2">
               <Image
                 src={coin.image}
                 alt={coin.name}
